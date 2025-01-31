1045aae0f63df6ba348fac8b10c02c2b8ee2c41b
Shaik_Rasool <157970061+merasool@users.noreply.github.com>
2025-01-31 13:53:09 +0000

    Pending changes exported from your codespace

---
+++ amstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a,t;
+    scanf("%d",&a);
+    while (a!=0)
+    {
+        a=a/10;
+        t++;
+        /* code */
+    }
+    while (a!=0)
+    {
+        
+        /* code */
+    }
+    
+    
+}
